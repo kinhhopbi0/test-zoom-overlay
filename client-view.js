79686c64a4e6482346e6f1156777c0dfe548f3c8
ducnv <ducnv@watermelon.vn>
2023-06-28 11:47:40 +0700

    add-code-client-view

--- client-view.js
+++ client-view.js
@@ -7,36 +7,46 @@ ZoomMtg.i18n.load('en-US')
 ZoomMtg.i18n.reload('en-US')
 
 var authEndpoint = ''
-var sdkKey = ''
-var meetingNumber = '123456789'
+var sdkKey = 'BJvHvXAETNelXgCaqayrRg'
+var meetingNumber = ''
 var passWord = ''
 var role = 0
 var userName = 'JavaScript'
 var userEmail = ''
 var registrantToken = ''
 var zakToken = ''
-var leaveUrl = 'https://zoom.us'
-
+var leaveUrl = 'https://zoom.us';
+const clientId = "BJvHvXAETNelXgCaqayrRg";
+const clientSecret = "u9TMk31Y17NgVM9P5KpDvVYX5IWUIh1z";
+document.getElementById('custom-area').style.display = 'none';
 function getSignature() {
-  fetch(authEndpoint, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      meetingNumber: meetingNumber,
-      role: role
-    })
-  }).then((response) => {
-    return response.json()
-  }).then((data) => {
-    console.log(data)
-    startMeeting(data.signature)
-  }).catch((error) => {
-  	console.log(error)
-  })
+  meetingNumber = document.getElementsByName("meeting_id")[0].value.replaceAll(" ", "");
+  passWord = document.getElementsByName("passcode")[0].value;
+  let signature = generateSignature(clientId, clientSecret, meetingNumber, role)
+  console.log(signature)
+  startMeeting(signature)
 }
+function generateSignature(key, secret, meetingNumber, role) {
+  // const KJUR = require('jsrsasign')
+  const iat = Math.round(new Date().getTime() / 1000) - 30
+  const exp = iat + 60 * 60 * 2
+  const oHeader = { alg: 'HS256', typ: 'JWT' }
 
+  const oPayload = {
+    sdkKey: key,
+    appKey: key,
+    mn: meetingNumber,
+    role: role,
+    iat: iat,
+    exp: exp,
+    tokenExp: exp
+  }
+
+  const sHeader = JSON.stringify(oHeader)
+  const sPayload = JSON.stringify(oPayload)
+  const sdkJWT = KJUR.jws.JWS.sign('HS256', sHeader, sPayload, secret)
+  return sdkJWT
+}
 function startMeeting(signature) {
 
   document.getElementById('zmmtg-root').style.display = 'block'
@@ -44,6 +54,7 @@ function startMeeting(signature) {
   ZoomMtg.init({
     leaveUrl: leaveUrl,
     success: (success) => {
+      document.getElementById('custom-area').style.display = 'inline-block';
       console.log(success)
       ZoomMtg.join({
         signature: signature,
@@ -52,8 +63,8 @@ function startMeeting(signature) {
         passWord: passWord,
         userName: userName,
         userEmail: userEmail,
-        tk: registrantToken,
-        zak: zakToken,
+        // tk: registrantToken,
+        // zak: zakToken,
         success: (success) => {
           console.log(success)
         },
